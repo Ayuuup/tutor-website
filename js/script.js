892b577dabf3bb52761a9ba0596f94a1881b2652
ayoub <babakhouyaayoub@gmail.com>
2025-06-03 15:44:23 +0100

    push content below when ham menu is active

--- js/script.js
+++ js/script.js
@@ -1,9 +1,20 @@
 const hamburgerButton = document.querySelector('.hamburger');
 const navMenu = document.querySelector('.nav-items');
+const nav = document.querySelector('nav');
+const hero = document.querySelector('.hero');
 hamburgerButton.addEventListener('click',openMenu);
 function openMenu(){
     navMenu.classList.toggle('active');
     hamburgerButton.classList.toggle('active')
+    let navHeight = nav.offsetHeight;
+
+    // If nav-items is active (visible), add its height too
+    if (navMenu.classList.contains('active')) {
+        navHeight += navMenu.offsetHeight;
+    }
+
+    hero.style.paddingTop = navHeight + 'px';
+
 }
 
 const navLinks = document.querySelectorAll('.nav-items li')
